9951678a9259abcc7c408385981222c1da49dcb2
Ali <4e5.kn@mail.ru>
2024-05-12 23:25:46 +0300

    туц

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { app } from "./app";
 import { SETTINGS } from "./seting/seting";
 import { dbT } from "./db/mongo-.db";
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const startApi = async () => {
-  await dbT.run(SETTINGS.MONGO_URL);
+  await dbT.run(process.env.MONGO_URL || SETTINGS.MONGO_URL);
 
   app.listen(SETTINGS.PORT, () => {
     console.log(`server   start`);
